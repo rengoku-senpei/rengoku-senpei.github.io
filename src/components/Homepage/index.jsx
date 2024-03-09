f483fbeadeb83b0ea16c8e9c90023b54134fcf2e
Ben <nanami-senpei>
2024-03-09 15:22:18 +0530

    Profile pic and other updates

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { DarkIco, EmailIcon, LightIco, PhoneIco } from './Icons';
@@ -36,10 +36,30 @@ const HomePage = () => {
     setIsLoadingImage(false);
   };
 
+  useEffect(() => {
+    if (
+      (window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+      localStorage.getItem('theme') === 'dark'
+    ) {
+      setDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
-    <div className="bg-cyan-50 min-h-screen">
+    <div className="bg-cyan-50 dark:dark:bg-slate-800 min-h-screen">
       <div className="h-full">
-        <nav className="flex lg:px-[80px] md:px-[30px] px-[15px] md:py-10 justify-between h-1/5 bg-cyan-700">
+        <nav className="flex lg:px-[80px] md:px-[30px] px-[15px] md:py-10 justify-between h-1/5 dark:bg-slate-900 bg-cyan-700">
           <div className="font-burtons lg:text-3xl md:text-xl text-lg h-100 flex flex-col justify-center text-white">
             Ben Mannathara
           </div>
@@ -48,34 +68,34 @@ const HomePage = () => {
               {darkMode ? <DarkIco /> : <LightIco />}
             </div>
             <div>
-              <button className="my-5 bg-white text-cyan-700 text-sm rounded-md md:px-5 md:py-2 py-1 px-3">
+              <button className="my-5 dark:bg-slate-700 dark:text-white bg-white text-cyan-700 text-sm rounded-md md:px-5 md:py-2 py-1 px-3">
                 Resume
               </button>
             </div>
           </div>
         </nav>
-        <main className="flex flex-col h-3/5">
-          <div className="flex flex-col md:py-10 py-4">
+        <main className="flex flex-col">
+          <div className="flex flex-col md:pt-10 pt-4">
             <div className="lg:h-56 md:h-40 h-32 flex justify-center mt-10">
               {isLoadingImage && (
                 <Skeleton
                   circle
-                  className="h-full rounded-full p-[2px] border-cyan-300 border-2 lg:!w-[210px] md:!w-[150px] !w-[120px]"
+                  className="h-full rounded-full p-[2px] dark:border-slate-400 border-cyan-300 border-2 lg:!w-[210px] md:!w-[150px] !w-[120px]"
                 />
               )}
               <img
-                className="h-full rounded-full p-[2px] border-cyan-300 border-2"
+                className="h-full rounded-full p-[2px] dark:border-slate-400 border-cyan-300 border-2"
                 src="./Me.jpg"
                 onLoad={handleImageLoad} // Handle image load event
                 style={{ display: isLoadingImage ? 'none' : 'block' }}
               />
             </div>
 
-            <div className="flex flex-col px-[20px] py-[20px] md:px-[80px] md:py-[40px] lg:px-[120px] lg:py-[40px] gap-2 bg-white mt-4">
-              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8">
+            <div className="flex flex-col px-[20px] py-[20px] md:px-[80px] md:py-[40px] lg:px-[120px] lg:py-[40px] gap-2 bg-white mt-4 dark:bg-slate-900">
+              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8 dark:text-slate-200">
                 Overview
               </div>
-              <div className="lg:py-5 lg:px-10 lg:text-xl md:text-base text-sm font-poppins text-cyan-700 indent-20 font-medium md:leading-8 leading-[20px]">
+              <div className="lg:py-5 lg:px-10 lg:text-xl md:text-base text-sm font-poppins text-cyan-700 indent-20 font-medium md:leading-8 leading-[20px] dark:text-slate-400">
                 As a seasoned React-Node web developer specializing in
                 full-stack development, I deliver exceptional services across
                 both frontend and backend domains. With expertise in crafting
@@ -89,43 +109,43 @@ const HomePage = () => {
               </div>
             </div>
             <div className="flex flex-col px-[20px] py-[10px] md:px-[80px] md:py-[40px] lg:px-[120px] lg:py-[40px] gap-2">
-              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8">
+              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8 dark:text-slate-200">
                 Services
               </div>
-              <div className="flex flex-col md:flex-row justify-between text-cyan-600 gap-5 flex-wrap px-10 md:p-4">
-                <div className="shadow-md shadow-cyan-200 md:w-[30%] px-10 py-6 md:p-3 flex flex-col items-center gap-3">
+              <div className="flex flex-col md:flex-row justify-between text-cyan-600  gap-5 flex-wrap px-10 md:p-4">
+                <div className="shadow-md dark:shadow-sm shadow-cyan-200 dark:shadow-slate-400 md:w-[30%] px-10 py-6 md:p-3 flex flex-col items-center gap-3">
                   <div className="h-24">
                     <img src="/vite.svg" className="h-full" />
                   </div>
-                  <div className="underline underline-offset-4 text-center font-burtons md:text-base text-sm">
+                  <div className="underline underline-offset-4 text-center font-burtons dark:text-slate-200 md:text-base text-sm">
                     Frontend Design
                   </div>
-                  <div className="text-sm md:text-base text-center">
+                  <div className="text-sm md:text-base text-center dark:text-slate-300">
                     Utilizing React and Vite to craft visually appealing and
                     interactive user interfaces for web applications.
                   </div>
                 </div>
-                <div className="shadow-md shadow-cyan-200 md:w-[30%] px-10 py-6 md:p-3 flex flex-col items-center gap-3">
+                <div className="shadow-md dark:shadow-sm shadow-cyan-200 dark:shadow-slate-400 md:w-[30%] px-10 py-6 md:p-3 flex flex-col items-center gap-3">
                   <div className="h-24">
                     <img src="/node.svg" className="h-full" />
                   </div>
-                  <div className="underline underline-offset-4 text-center font-burtons md:text-base text-sm">
+                  <div className="underline underline-offset-4 text-center font-burtons dark:text-slate-200 md:text-base text-sm">
                     Backend Development
                   </div>
-                  <div className="text-sm md:text-base text-center">
+                  <div className="text-sm md:text-base text-center dark:text-slate-300">
                     Employing Node.js or NestJS to architect and implement
                     robust server-side solutions, ensuring efficient data
                     processing and API functionality.
                   </div>
                 </div>
-                <div className="shadow-md shadow-cyan-200 md:w-[30%] px-10 py-6 md:p-3  flex flex-col items-center gap-3">
+                <div className="shadow-md dark:shadow-sm shadow-cyan-200 dark:shadow-slate-400 md:w-[30%] px-10 py-6 md:p-3  flex flex-col items-center gap-3">
                   <div className="h-24">
                     <img src="/maintenance.svg" className="h-full" />
                   </div>
-                  <div className="underline underline-offset-4 text-center font-burtons md:text-base text-sm">
+                  <div className="underline underline-offset-4 text-center font-burtons dark:text-slate-200 md:text-base text-sm">
                     Maintenance
                   </div>
-                  <div className="text-sm md:text-base text-center">
+                  <div className="text-sm md:text-base text-center dark:text-slate-300">
                     Continuously monitoring and updating both frontend and
                     backend components to ensure seamless operation and optimal
                     performance of the web application.
@@ -133,14 +153,14 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col px-[20px] py-[20px] md:px-[80px] md:py-[40px] lg:px-[120px] lg:py-[40px] gap-5 bg-white mt-4">
-              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8">
+            <div className="flex flex-col py-[20px] md:px-[80px] md:py-[40px] lg:px-[120px] lg:py-[40px] gap-5 bg-white dark:bg-slate-900 mt-4">
+              <div className="text-cyan-700 md:text-2xl text-lg font-burtons  text-center underline underline-offset-8 dark:text-slate-200">
                 Familiar Technologies/Skills
               </div>
               <div className="flex gap-3 flex-wrap items-center justify-center">
                 {skills.map((e, i) => (
                   <div
-                    className="border border-cyan-500 md:p-2 p-1 rounded-md text-cyan-900 md:w-[150px] text-center shadow-md shadow-cyan-200 w-[110px] md:text-base text-sm"
+                    className="border dark:border-slate-400 border-cyan-500 md:p-2 p-1 rounded-md dark:text-slate-300 text-cyan-900 md:w-[150px] text-center shadow-md shadow-cyan-200 dark:shadow-slate-500 w-[110px] md:text-base text-sm dark:shadow-sm"
                     key={i}
                   >
                     {e}
@@ -150,9 +170,9 @@ const HomePage = () => {
             </div>
           </div>
         </main>
-        <footer className="bg-cyan-900 flex flex-col md:h-[24vh] h-[15vh] md:p-8 p-4 justify-center text-sm md:text-base">
+        <footer className="bg-cyan-900 dark:bg-slate-700 flex flex-col md:h-[24vh] h-[15vh] md:p-8 p-4 justify-center text-sm md:text-base">
           <div className="flex flex-col md:gap-2 gap-1">
-            <div className="text-white underline underline-offset-4">
+            <div className="text-white underline underline-offset-4 font-burtons">
               Contact Me
             </div>
             <div className="flex gap-2 text-white">
